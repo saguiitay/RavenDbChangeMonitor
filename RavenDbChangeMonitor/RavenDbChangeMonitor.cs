70934ca4b89b225a35c98b593806e3ec4c679884
Itay Sagui <saguiitay@hotmail.com>
2015-06-19 16:32:15 +0300

    initial commit

---
+++ RavenDbChangeMonitor/RavenDbChangeMonitor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+using Raven.Client.Changes;
+
+namespace RavenDbChangeMonitor
+{
+    public class RavenDbChangeMonitor<T> : ChangeMonitor
+        where T : EventArgs
+    {
+        private readonly List<IDisposable> _subscribtions = new List<IDisposable>();
+
+        public RavenDbChangeMonitor(params IObservableWithTask<T>[] observableWithTasks)
+        {
+            UniqueId = Guid.NewGuid().ToString();
+            var observer = new Observer(this);
+
+            foreach (var observableWithTask in observableWithTasks)
+            {
+                var subscription = observableWithTask.Subscribe(observer);
+                _subscribtions.Add(subscription);
+            }
+
+            InitializationComplete();
+        }
+
+        #region Overrides of ChangeMonitor
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (var subscription in _subscribtions)
+                    subscription.Dispose();
+            }
+        }
+
+        public override string UniqueId { get; }
+
+        #endregion
+
+        private class Observer : IObserver<T>
+        {
+            private readonly RavenDbChangeMonitor<T> _monitor;
+
+            public Observer(RavenDbChangeMonitor<T> monitor)
+            {
+                _monitor = monitor;
+            }
+
+            #region Implementation of IObserver<in DocumentChangeNotification>
+
+            public void OnNext(T value)
+            {
+                _monitor.OnChanged(value);
+            }
+
+            public void OnError(Exception error) { }
+
+            public void OnCompleted() { }
+
+            #endregion
+        }
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Abstractions.Data;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDocumentStore store = new DocumentStore
+                {
+                    Url = "http://localhost:8080/",
+                    DefaultDatabase = "SampleDatabase"
+                }.Initialize();
+
+            using (var session = store.OpenSession())
+            {
+                var document = new
+                    {
+                        Id = "documents/1",
+                        Property = "Value1"
+                    };
+                session.Store(document);
+                session.SaveChanges();
+            }
+
+            var cache = MemoryCache.Default;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+
+            var changes = store.Changes().ForDocument("documents/1");
+            policy.ChangeMonitors.Add(new RavenDbChangeMonitor.RavenDbChangeMonitor<DocumentChangeNotification>(changes));
+            policy.UpdateCallback = arguments =>
+                {
+                    var key = arguments.Key;
+                    Console.WriteLine("Cache object (with key {0}) was updated", key);
+                };
+
+            cache.Set("cacheKey", "cacheValue", policy);
+
+
+            Console.ReadLine();
+        }
+    }
+}
